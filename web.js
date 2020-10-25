5dae2cf840e575e2cd6d2284d05e726f733754af
r4m1m <ramimgc8@gmail.com>
2020-10-25 23:49:49 +0600

    first commit

---
+++ web.js
@@ -0,0 +1,34 @@
+console.log('hello world')
+
+//alert('you are the mother faka')
+
+//varialbles
+
+//var name = 's a i f u l  i s l a m';
+
+//console.log(name);
+
+//let age = prompt('what is your age?');
+//document.getElementById('s').innerHTML = age;
+
+
+//console.log(age);
+
+//FUNCTIONS
+
+/*function name(){
+    let name = prompt('whats your name?');
+    let i = 1;
+    while(i < 100){
+        document.getElementById('s').innerHTML = i + 'HELLO ' + name;
+        i++;
+    }
+    //for(let j=0;j<100;j++)console.log(j);
+}
+name();*/
+
+
+//string s
+
+let fruit = 'apple\nbanna';
+console.log(fruit);
